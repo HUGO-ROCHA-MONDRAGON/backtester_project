3c012ad89672d13256e60af704fa294f0f39c7b6
Hugo <hugosrochamo@gmail.com>
2025-10-13 20:58:41 +0000

    Fin

---
+++ backtester_project/src/backtester_project/strategies/__init__.py
@@ -0,0 +1,5 @@
+from .buy_and_hold import BuyAndHold
+from .momentum import MomentumStrategy
+from .mean_reversion import MeanReversionStrategy
+
+__all__ = ["BuyAndHold", "MomentumStrategy", "MeanReversionStrategy"]
